80f8532b544f525b8d7bc2971d76555fab02a622
UG-Peeroo <Kaleem.Peeroo@city.ac.uk>
2019-07-17 15:21:32 +0100

    Changed file names

--- test-1.2-sub-2.3.bat
+++ test-1.2-sub-2.3.bat
@@ -1,1 +1,1 @@
-start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 5 -domain 1 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub1 -sidMultiSubTest 5
+start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 5 -domain 1 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub5 -sidMultiSubTest 5
