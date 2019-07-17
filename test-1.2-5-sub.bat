906d67aa85c284148348b4d57e45f25dab68d99e
UG-Peeroo <Kaleem.Peeroo@city.ac.uk>
2019-07-17 18:49:35 +0100

    Added 5 sub script

---
+++ test-1.2-5-sub.bat
@@ -0,0 +1,9 @@
+start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 5 -domain 1 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub1 -sidMultiSubTest 1
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 5 -domain 1 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub2 -sidMultiSubTest 2
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 5 -domain 1 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub3 -sidMultiSubTest 3
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 5 -domain 1 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub4 -sidMultiSubTest 4
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 5 -domain 1 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub5 -sidMultiSubTest 5
