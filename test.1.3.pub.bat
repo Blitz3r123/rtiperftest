7805658432c95e4a85ba64e8bc20f09fba4a8e8e
BuildTools <unconfigured@null.spigotmc.org>
2019-07-20 14:41:39 +0100

    Added more test scripts

---
+++ test.1.3.pub.bat
@@ -0,0 +1,1 @@
+start cmd /k perftest_java.bat -pub -dataLen 512 -numSubscribers 1 -domain 1 -latencyCount 10000 -numIter 10000000 -multicast
---
+++ test.1.6.pub.bat
@@ -0,0 +1,1 @@
+start cmd /k perftest_java.bat -pub -dataLen 512 -numSubscribers 5 -domain 1 -latencyCount 10000 -numIter 10000000 -bestEffort
