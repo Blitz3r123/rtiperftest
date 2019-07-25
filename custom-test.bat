475749bd23b9942a192a6a11c436a42d41944fba
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-07-25 12:56:26 +0100

    BUilt code and edited custom test

--- custom-test.bat
+++ custom-test.bat
@@ -1,2 +1,3 @@
-start cmd /k perftest_java.bat -pub -dataLen 512 -numSubscribers 1 -domain 1 -latencyCount 10000 -executionTime 120 -bestEffort -multicast
-start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 1 -domain 1 -latencyCount 10000 -executionTime 120 -bestEffort -multicast -fileName sub1 -sidMultiSubTest 174
+start cmd /k perftest_java.bat -pub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 10000 -executionTime 20 -bestEffort -multicast
+start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 10000 -executionTime 20 -bestEffort -multicast -fileName sub1 -sidMultiSubTest 1
+start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 10000 -executionTime 20 -bestEffort -multicast -fileName sub1 -sidMultiSubTest 2
