5fb6b827def93c6df889697e04d66c0fa1127dc2
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-07-25 12:59:38 +0100

    Renamed file to sub2 for custom test

--- custom-test.bat
+++ custom-test.bat
@@ -1,3 +1,3 @@
 start cmd /k perftest_java.bat -pub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 100 -executionTime 20 -bestEffort -multicast
 start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 100 -executionTime 20 -bestEffort -multicast -fileName sub1 -sidMultiSubTest 1
-start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 100 -executionTime 20 -bestEffort -multicast -fileName sub1 -sidMultiSubTest 2
+start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 100 -executionTime 20 -bestEffort -multicast -fileName sub2 -sidMultiSubTest 2
