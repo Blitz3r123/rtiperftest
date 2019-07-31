1b6efd4a9082250b55bfe2d37b3a55855452e9e9
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-07-31 10:28:57 +0100

    Updated t.bat with numiter 1000 and latencycount 10

--- t.bat
+++ t.bat
@@ -1,2 +1,2 @@
-start cmd /k perftest_java.bat -pub -latencyTest -latencyCount 10000 -numIter 500000
+start cmd /k perftest_java.bat -pub -latencyTest -latencyCount 10 -numIter 10000
 start cmd /k perftest_java.bat -sub -fileName sub1
