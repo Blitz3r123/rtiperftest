c313623b82b9c861ad1005fe73b59f8380268218
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-08-01 15:39:13 +0100

    CHanged test scripts

--- custom-test.bat
+++ custom-test.bat
@@ -1,3 +1,5 @@
-start cmd /k perftest_java.bat -pub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 100 -executionTime 20 -bestEffort -multicast
-start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 100 -executionTime 20 -bestEffort -multicast -fileName sub1 -sidMultiSubTest 1
-start cmd /k perftest_java.bat -sub -dataLen 512 -numSubscribers 2 -domain 1 -latencyCount 100 -executionTime 20 -bestEffort -multicast -fileName sub2 -sidMultiSubTest 2
+rm "./pub.csv"
+rm "./sub1.csv"
+start cmd /k perftest_java.bat -pub -latencyCount 10 -numIter 10000
+start /W cmd /k perftest_java.bat -sub -fileName sub1
+echo "ALL DONE"
