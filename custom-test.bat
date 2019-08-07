6aa1e7e8a5aaf9301116dacdf7b69fbe73b560bc
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-08-07 15:10:18 +0100

    Updated custom-test.bat

--- custom-test.bat
+++ custom-test.bat
@@ -1,5 +1,2 @@
-rm "./pub.csv"
-rm "./sub1.csv"
 start cmd /k perftest_java.bat -pub -latencyCount 10 -numIter 10000
-start /W cmd /k perftest_java.bat -sub -fileName sub1
-echo "ALL DONE"
+start cmd /k perftest_java.bat -sub -fileName sub1
