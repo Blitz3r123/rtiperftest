7c89bd0e42dee414248e2e7c059ffb3b4acafd04
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-08-07 15:11:39 +0100

    Updated custom-test

--- custom-test.bat
+++ custom-test.bat
@@ -1,2 +1,2 @@
-start cmd /k perftest_java.bat -pub -latencyCount 10 -numIter 10000
+start cmd /k perftest_java.bat -pub -latencyCount 10 -numIter 10000 -dataLen 512
 start cmd /k perftest_java.bat -sub -fileName sub1
