dcafd3820ab3100c471b5f84781f427fafddb22b
Kaleem <kaleem.peeroo@city.ac.uk>
2019-08-19 20:37:18 +0100

    Edited test 1.1 scripts!

--- test.1.1.pub.bat
+++ test.1.1.pub.bat
@@ -1,1 +1,1 @@
-start cmd /k perftest_java.bat -pub -dataLen 4096 -numSubscribers 1 -domain 2 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast
+start cmd /k perftest_java.bat -pub -dataLen 4096 -domain 2 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast
