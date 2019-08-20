269b9c0ccfda688709cb7b2d1de8838b9c181da3
Kaleem <kaleem.peeroo@city.ac.uk>
2019-08-20 10:59:23 +0100

    Edited custom test

--- custom-test.bat
+++ custom-test.bat
@@ -1,2 +1,2 @@
-start cmd /k perftest_java.bat -pub -latencyCount 10 -numIter 1000000 -dataLen 512
-start cmd /k perftest_java.bat -sub -fileName sub1
+start cmd /k perftest_java.bat -pub -dataLen 4096 -domain 2 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast
+start cmd /k perftest_java.bat -sub -dataLen 4096 -domain 2 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub1
