0c10723ea87c2df714b747946e37917db91deafa
Kaleem <kaleem.peeroo@city.ac.uk>
2019-09-20 18:32:33 +0100

    Rearranged all tests

--- test.1.10.sub.1.2.bat
+++ test.1.10.sub.1.2.bat
@@ -1,5 +1,9 @@
-start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub1 -sidMultiSubTest 1 -bestEffort
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub6 -sidMultiSubTest 6 -bestEffort
 
-start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub2 -sidMultiSubTest 2 -bestEffort
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub7 -sidMultiSubTest 7 -bestEffort
 
-start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub3 -sidMultiSubTest 3 -bestEffort
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub8 -sidMultiSubTest 8 -bestEffort
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub9 -sidMultiSubTest 9 -bestEffort
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub10 -sidMultiSubTest 10 -bestEffort
