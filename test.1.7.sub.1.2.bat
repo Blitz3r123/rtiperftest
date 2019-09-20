bfa760e13c6d211cc7efb3af098e7fd1e417a1dc
Kaleem <kaleem.peeroo@city.ac.uk>
2019-09-20 18:24:25 +0100

    Fixed some bugs

--- test.1.7.sub.1.2.bat
+++ test.1.7.sub.1.2.bat
@@ -1,5 +1,5 @@
-start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub1 -sidMultiSubTest 3 -multicast
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub3 -sidMultiSubTest 3 -multicast
 
-start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub2 -sidMultiSubTest 4 -multicast
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub4 -sidMultiSubTest 4 -multicast
 
-start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub2 -sidMultiSubTest 5 -multicast
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub5 -sidMultiSubTest 5 -multicast
