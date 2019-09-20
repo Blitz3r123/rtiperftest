dcb964fa1c12e75beac24f5af9b66e6176fe97a8
Kaleem <kaleem.peeroo@city.ac.uk>
2019-09-20 18:28:12 +0100

    Fixed more bugs

--- test.1.5.sub.1.2.bat
+++ test.1.5.sub.1.2.bat
@@ -2,4 +2,4 @@ start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -
 
 start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub4 -sidMultiSubTest 4
 
-start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub4 -sidMultiSubTest 5
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -fileName sub5 -sidMultiSubTest 5
---
+++ test.1.9.sub.1.1.bat
@@ -0,0 +1,9 @@
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub1 -sidMultiSubTest 1 -bestEffort -multicast
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub2 -sidMultiSubTest 2 -bestEffort -multicast
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub3 -sidMultiSubTest 3 -bestEffort -multicast
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub4 -sidMultiSubTest 4 -bestEffort -multicast
+
+start cmd /k perftest_java.bat -sub -dataLen 512 -domain 2 -latencyCount 10000 -numIter 10000000 -fileName sub5 -sidMultiSubTest 5 -bestEffort -multicast
