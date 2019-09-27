16f8fc27da6c6c63faa6ee25cccbdb161eba0b2c
Kaleem <kaleem.peeroo@city.ac.uk>
2019-09-27 19:54:17 +0100

    Changed num iter to 1000000

--- custom-test-pub.bat
+++ custom-test-pub.bat
@@ -1,1 +1,1 @@
-start cmd /k perftest_java.bat -pub -dataLen 4096 -domain 2 -latencyCount 10000 -numIter 10000000 -bestEffort -multicast -instances 10
+start cmd /k perftest_java.bat -pub -dataLen 4096 -domain 2 -latencyCount 10000 -numIter 1000000 -bestEffort -multicast -instances 10
