f82d280f2b85483533f39e63744186e18a44c4a2
Kaleem <kaleem.peeroo@city.ac.uk>
2019-10-31 18:24:09 +0000

    Updated file output names for all tests

--- test.6.sub.2.bat
+++ test.6.sub.2.bat
@@ -1,3 +1,3 @@
-start cmd /k perftest_java.bat -sub -instances 10 -keyed -dataLen 40960 -domain 2 -bestEffort -fileName subscriber -multicast -sidMultiSubTest 3
+start cmd /k perftest_java.bat -sub -instances 10 -keyed -dataLen 40960 -domain 2 -bestEffort -fileName subscriber3 -multicast -sidMultiSubTest 3
 
-start cmd /k perftest_java.bat -sub -instances 10 -keyed -dataLen 40960 -domain 2 -bestEffort -fileName subscriber -multicast -sidMultiSubTest 4
+start cmd /k perftest_java.bat -sub -instances 10 -keyed -dataLen 40960 -domain 2 -bestEffort -fileName subscriber4 -multicast -sidMultiSubTest 4
