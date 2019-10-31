df801e8e9ff8d7d3e080b8f59bf75587c4ec2390
Kaleem <kaleem.peeroo@city.ac.uk>
2019-10-31 18:05:19 +0000

    Added test 5

---
+++ test.5.sub.1.bat
@@ -0,0 +1,3 @@
+start cmd /k perftest_java.bat -sub -instances 10 -keyed -dataLen 40960 -domain 2 -bestEffort -fileName subscriber -sidMultiSubTest 1
+
+start cmd /k perftest_java.bat -sub -instances 10 -keyed -dataLen 40960 -domain 2 -bestEffort -fileName subscriber -sidMultiSubTest 2
---
+++ test.5.sub.3.bat
@@ -0,0 +1,2 @@
+start cmd /k perftest_java.bat -sub -instances 10 -keyed -dataLen 40960 -domain 2 -bestEffort -fileName subscriber -sidMultiSubTest 5
+
