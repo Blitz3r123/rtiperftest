aa782a1e33c339698b10dfc78325946f96479da0
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-11-05 17:26:05 +0000

    Fixed bug in publishers

--- test.1.pub.4.bat
+++ test.1.pub.4.bat
@@ -1,3 +1,3 @@
-start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 40980 -domain 2 -latencyCount 1000 -numIter 1000000 -bestEffort -sendQueueSize 1 -pidMultiSubTest 4 -numSubscribers 1
+start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 40980 -domain 2 -latencyCount 1000 -numIter 1000000 -bestEffort -sendQueueSize 1 -pidMultiPubTest 4 -numSubscribers 1
 
-start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 40980 -domain 2 -latencyCount 1000 -numIter 1000000 -bestEffort -sendQueueSize 1 -pidMultiSubTest 5 -numSubscribers 1
+start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 40980 -domain 2 -latencyCount 1000 -numIter 1000000 -bestEffort -sendQueueSize 1 -pidMultiPubTest 5 -numSubscribers 1
