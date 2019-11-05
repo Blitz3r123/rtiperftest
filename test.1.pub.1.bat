f6b1a464914284f5f37e70c9067ddf2360ccd519
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-11-05 17:31:29 +0000

    Added onPrintIntervals to scripts

--- test.1.pub.1.bat
+++ test.1.pub.1.bat
@@ -1,1 +1,1 @@
-start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 40980 -domain 2 -latencyCount 1000 -numIter 1000000 -bestEffort -sendQueueSize 1 -pidMultiPubTest 1 -numSubscribers 1
+start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 40980 -domain 2 -latencyCount 1000 -numIter 1000000 -bestEffort -sendQueueSize 1 -pidMultiPubTest 1 -numSubscribers 1 -noPrintIntervals
