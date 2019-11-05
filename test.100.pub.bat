5b8763ad77120588a8bc2265fc6f637e5bca18c8
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2019-11-05 18:34:26 +0000

    Removed no print intervals

--- test.100.pub.bat
+++ test.100.pub.bat
@@ -1,1 +1,1 @@
-start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 100 -domain 2 -latencyCount 1000 -numIter 1000000 -sendQueueSize 100 -noPrintIntervals -multicast
+start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 100 -domain 2 -latencyCount 1000 -numIter 1000000 -sendQueueSize 100 -multicast
