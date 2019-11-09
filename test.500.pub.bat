b4f22b3bea64307bc2d5f41b02ccc88d5bb36a1e
Kaleem <kaleem.peeroo@city.ac.uk>
2019-11-09 07:23:10 +0000

    Changed tests to reliable

--- test.500.pub.bat
+++ test.500.pub.bat
@@ -1,1 +1,1 @@
-start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 40980 -domain 2 -latencyCount 1000 -numIter 1000000 -sendQueueSize 500 -multicast
+start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 40980 -domain 2 -latencyCount 1000 -numIter 1000000 -sendQueueSize 500
