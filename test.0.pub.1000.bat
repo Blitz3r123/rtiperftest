15e3329372f97575f368603cdfe00921c4f6671e
Kaleem <kaleem.peeroo@city.ac.uk>
2019-11-14 09:05:21 +0000

    Added reliable unicast unkeyed tests for varying queue size test

--- test.0.pub.1000.bat
+++ test.0.pub.1000.bat
@@ -1,1 +1,1 @@
-start cmd /k perftest_java.bat -pub -dataLen 100 -domain 1 -latencyCount 1000 -numIter 1000000 -bestEffort -sendQueueSize 1000
+start cmd /k perftest_java.bat -pub -dataLen 100 -domain 1 -latencyCount 1000 -numIter 1000000 -sendQueueSize 1000
