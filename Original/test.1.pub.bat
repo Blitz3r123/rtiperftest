efcb9ed103200c9abc979bbd2e86bccfade48e27
UG-Peeroo <Kaleem.Peeroo@city.ac.uk>
2019-11-14 07:42:35 +0000

    Added tests setup to original folder

---
+++ Original/test.1.pub.bat
@@ -0,0 +1,1 @@
+start cmd /k perftest_java.bat -pub -instances 1 -keyed -dataLen 100 -domain 2 -latencyCount 1000 -numIter 1000000 -bestEffort -sendQueueSize 1
---
+++ Original/test.1.sub.bat
@@ -0,0 +1,1 @@
+start cmd /k perftest_java.bat -sub -dataLen 40960 -domain 2 -bestEffort -fileName subscriber -numPublishers 5
