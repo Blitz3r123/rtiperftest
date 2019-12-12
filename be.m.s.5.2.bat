2882f36608318b5d4880590248b0c97762eca9ef
Kaleem <kaleem.peeroo@city.ac.uk>
2019-12-12 16:17:44 +0000

    Added best effort multicast tests

---
+++ be.m.s.5.2.bat
@@ -0,0 +1,2 @@
+start cmd /k perftest_java.bat -sub -dataLen 100 -domain 1 -fileName sub2 -bestEffort -multicast -sidMultiSubTest 2
+start cmd /k perftest_java.bat -sub -dataLen 100 -domain 1 -fileName sub3 -bestEffort -multicast -sidMultiSubTest 3
