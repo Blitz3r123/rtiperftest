bbe5f39f60cce23bd1a5f9f2dc42571d17cd871a
Kaleem <kaleem.peeroo@city.ac.uk>
2019-12-23 17:24:49 +0000

    Changed numIter to 100,000,000 and latency count to 100,000

--- be.u.p.10.bat
+++ be.u.p.10.bat
@@ -1,1 +1,1 @@
-start cmd /k perftest_java.bat -pub -dataLen 100 -domain 1 -latencyCount 1000 -numIter 1000000 -sendQueueSize 10 -batchSize 0 -bestEffort   -numSubscribers 10
+start cmd /k perftest_java.bat -pub -dataLen 100 -domain 1 -latencyCount 100000 -numIter 100000000 -sendQueueSize 10 -batchSize 0 -bestEffort   -numSubscribers 10
