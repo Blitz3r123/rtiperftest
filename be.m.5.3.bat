4ca96d0af0ac01a437478ee0b5c8eb3196b13566
Kaleem <kaleem.peeroo@city.ac.uk>
2020-01-25 08:48:15 +0000

    Fixed spelling mistake in scripts

--- be.m.5.3.bat
+++ be.m.5.3.bat
@@ -1,3 +1,3 @@
 start cmd /k perftest_java.bat -pub -dataLen 100 -domain 1 -latencyCount 100000 -numIter 100000000 -sendQueueSize 10 -batchSize 0 -bestEffort -multicast -pidMultiPubTest 2
 
-start cmd /k perftest_java.bat -sub -dataLen 100 -domain 1 -fileName sub1 -bestEffort -multicast -numPublishers 5 -sideMultiSubTest 1
+start cmd /k perftest_java.bat -sub -dataLen 100 -domain 1 -fileName sub1 -bestEffort -multicast -numPublishers 5 -sidMultiSubTest 1
