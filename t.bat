b8abcd4797d365f400317e6581f86cfc3fddf59a
Blitz3r123 <kaleem.peeroo@city.ac.uk>
2020-01-28 11:01:10 +0000

    Built

--- t.bat
+++ t.bat
@@ -1,4 +1,4 @@
 rm './pub.csv'
 rm './sub1.csv'
-start cmd /k perftest_java.bat -pub -dataLen 28 -domain 2 -latencyCount 100 -numIter 10000000 -bestEffort -multicast
-start cmd /k perftest_java.bat -sub -dataLen 28 -domain 2 -latencyCount 100 -numIter 10000000 -bestEffort -multicast -fileName sub1
+start cmd /k perftest_java.bat -pub -dataLen 28 -domain 2 -latencyCount 100 -executionTime 20 -bestEffort -multicast
+start cmd /k perftest_java.bat -sub -dataLen 28 -domain 2 -latencyCount 100 -executionTime 20 -bestEffort -multicast -fileName sub1
