c39eaa42c1831e4db419bca7d650fc091e35ebeb
Kaleem <kaleem.peeroo@city.ac.uk>
2020-02-08 07:37:40 +0000

    Added reliable tests scripts

---
+++ r.u.s.10.2.bat
@@ -0,0 +1,3 @@
+start cmd /k perftest_java.bat -sub -dataLen 100 -domain 1 -fileName sub3 -sidMultiSubTest 3
+start cmd /k perftest_java.bat -sub -dataLen 100 -domain 1 -fileName sub4 -sidMultiSubTest 4
+start cmd /k perftest_java.bat -sub -dataLen 100 -domain 1 -fileName sub5 -sidMultiSubTest 5
